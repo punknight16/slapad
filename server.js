251d1c6bbb5891b1132a161bef1b9bbd6e78e6cd
punknight <punknight16@gmail.com>
2019-03-09 14:28:37 -0800

    update routes based on documentation

--- server.js
+++ server.js
@@ -9,14 +9,35 @@ var server = http.createServer(function(req, res){
 		case 'documentation':
 			res.end('documentation');
 			break;
+		case 'splash':
+			res.end('splash');
+			break;
 		case 'register':
 			res.end('register');
 			break;
 		case 'login': 
 			res.end('login');
 			break;
-		case 'home':
-			res.end('home');
+		case '/onboard-advertiser':
+			res.end('onboard-advertiser')
+			break;
+		case '/onboard-retailer':
+			res.end('/onboard-retailer');
+			break;
+		case '/create-shipping-label':
+			res.end('/create-shipping-label');
+			break;
+		case '/users':
+			res.end('/users');
+			break;
+		case '/coupons':
+			res.end('/coupons');
+			break;
+		case '/shipping-quotes':
+			res.end('/shipping-quotes');
+			break;
+		case '/printed-labels':
+			res.end('printed-labels');
 			break;
 		case 'logout':
 			res.end('logout');
